53e66839e5b529506848e18b653b53c26c0f358d
Thomas Zhang <mier.cat@mier.cat>
2023-03-05 03:22:14 -0800

    feat: lab 10 draft

--- notuber/index.js
+++ notuber/index.js
@@ -1,62 +1,161 @@
-let map;
+const API_URL = "https://jordan-marsh.herokuapp.com/rides";
+const API_USERNAME = "jLttbNzY";
 
-const SOUTH_STATION = { lat: 42.352271, lng: -71.055242 };
-const ICON_BASE = "./";
+const ICON_BASE = "./assets/icons/";
+const ICONS = {
+  car: {
+    icon: ICON_BASE + "car.png",
+  },
+  cat: {
+    icon: ICON_BASE + "cat.png",
+  },
+};
+
+const DEFAULT_CENTER = { lat: 42.352271, lng: -71.055242 };
+
+function toMiles(meters) {
+  return meters * 0.000621371192;
+}
+
+function toQueryString(params) {
+  return Object.keys(params)
+    .map((key) => `${key}=${params[key]}`)
+    .join("&");
+}
+
+function sendRequest(method, url, data) {
+  const promise = new Promise((resolve, reject) => {
+    const xhr = new XMLHttpRequest();
+    xhr.open(method, url);
+
+    xhr.responseType = "json";
+
+    if (method === "POST" && data) {
+      xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+    }
+
+    xhr.onload = () => {
+      if (xhr.status >= 400) {
+        reject(xhr.response);
+      } else {
+        resolve(xhr.response);
+      }
+    };
+
+    xhr.onerror = () => {
+      reject("Something went wrong");
+    };
+
+    xhr.send(toQueryString(data));
+  });
+
+  return promise;
+}
+
+let map, infoWindow, currentPosition;
+
+function handleLocationError(browserHasGeolocation, infoWindow, pos, msg) {
+  infoWindow.setPosition(pos);
+  infoWindow.setContent(
+    browserHasGeolocation
+      ? `Error: The Geolocation service failed. ${msg}`
+      : "Error: Your browser doesn't support geolocation."
+  );
+  infoWindow.open(map);
+}
 
 function initMap() {
   map = new google.maps.Map(document.getElementById("map"), {
-    center: new google.maps.LatLng(SOUTH_STATION.lat, SOUTH_STATION.lng),
+    center: new google.maps.LatLng(DEFAULT_CENTER.lat, DEFAULT_CENTER.lng),
     zoom: 14,
   });
+  infoWindow = new google.maps.InfoWindow();
 
-  const icons = {
-    car: {
-      icon: ICON_BASE + "car.png",
-    },
-  };
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(
+      ({ coords }) => {
+        currentPosition = {
+          lat: coords.latitude,
+          lng: coords.longitude,
+        };
 
-  const features = [
-    {
-      title: "mXfkjrFw",
-      position: new google.maps.LatLng(42.3453, -71.0464),
-      type: "car",
-    },
-    {
-      title: "nZXB8ZHz",
-      position: new google.maps.LatLng(42.3662, -71.0621),
-      type: "car",
-    },
-    {
-      title: "Tkwu74WC",
-      position: new google.maps.LatLng(42.3603, -71.0547),
-      type: "car",
-    },
-    {
-      title: "5KWpnAJN",
-      position: new google.maps.LatLng(42.3472, -71.0802),
-      type: "car",
-    },
-    {
-      title: "uf5ZrXYw",
-      position: new google.maps.LatLng(42.3663, -71.0544),
-      type: "car",
-    },
-    {
-      title: "VMerzMH8",
-      position: new google.maps.LatLng(42.3542, -71.0704),
-      type: "car",
-    },
-  ];
+        map.setCenter(currentPosition);
+        new google.maps.Marker({
+          position: currentPosition,
+          icon: ICONS["cat"].icon,
+          map: map,
+        });
 
-  // Create markers.
-  const markers = features.map(
-    (feature) =>
-      new google.maps.Marker({
-        position: feature.position,
-        icon: icons[feature.type].icon,
-        map: map,
-      })
-  );
+        sendRequest("POST", API_URL, {
+          username: API_USERNAME,
+          lat: currentPosition.lat,
+          lng: currentPosition.lng,
+        })
+          .then((responseData) => {
+            console.log(responseData);
+
+            // Show cars
+            const cars = responseData
+              .map(({ username, lat, lng }) => {
+                const carPosition = new google.maps.LatLng(lat, lng);
+                const distance =
+                  google.maps.geometry.spherical.computeDistanceBetween(
+                    currentPosition,
+                    carPosition
+                  );
+                return {
+                  title: username,
+                  position: carPosition,
+                  type: "car",
+                  distance: toMiles(distance).toFixed(2),
+                };
+              })
+              .sort((a, b) => a.distance - b.distance);
+
+            console.log(cars);
+
+            const markers = cars.map((car) => {
+              const marker = new google.maps.Marker({
+                position: car.position,
+                icon: ICONS["car"].icon,
+                map: map,
+              });
+
+              marker.addListener("click", () => {
+                infoWindow.setContent(
+                  `<div>
+                    <h3>${car.title}</h3>
+                    <p>Distance: ${car.distance} miles away</p>
+                  </div>`
+                );
+                infoWindow.open({ anchor: marker, map });
+              });
+
+              return marker;
+            });
+
+            const closestMarker = markers[0];
+            const line = new google.maps.Polyline({
+              path: [currentPosition, closestMarker.position],
+              geodesic: true,
+              strokeColor: "#FF0000",
+              strokeOpacity: 1.0,
+              strokeWeight: 2,
+            });
+            line.setMap(map);
+          })
+          .catch((err) => {
+            console.log(err);
+          });
+      },
+      (err) => {
+        handleLocationError(true, infoWindow, map.getCenter(), err.message);
+      }
+    );
+  } else {
+    // Browser doesn't support Geolocation
+    handleLocationError(false, infoWindow, map.getCenter());
+  }
 }
 
 window.initMap = initMap;
