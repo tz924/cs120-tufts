c4c9a728cc13df9622c60b42ea1e626e365e3537
Thomas Zhang <mier.cat@mier.cat>
2023-04-09 08:06:44 -0700

    feat: created alternative reality

--- notuber/frontend/index.js
+++ notuber/frontend/index.js
@@ -17,6 +17,8 @@ const ICONS = {
 const DEFAULT_CENTER = { lat: 42.352271, lng: -71.055242 };
 const MILE = 1609.344;
 
+let map, infoWindow, currentPosition;
+
 function toMiles(meters) {
   return (meters * 0.000621371192).toFixed(2);
 }
@@ -31,7 +33,7 @@ function toQueryString(params) {
     .join("&");
 }
 
-function sendRequest(method, url, data) {
+async function sendRequest(method, url, data) {
   const promise = new Promise((resolve, reject) => {
     const xhr = new XMLHttpRequest();
     xhr.open(method, url);
@@ -60,8 +62,6 @@ function sendRequest(method, url, data) {
   return promise;
 }
 
-let map, infoWindow, currentPosition;
-
 function handleLocationError(browserHasGeolocation, infoWindow, pos, msg) {
   infoWindow.setPosition(pos);
   infoWindow.setContent(
@@ -72,17 +72,20 @@ function handleLocationError(browserHasGeolocation, infoWindow, pos, msg) {
   infoWindow.open(map);
 }
 
-function initMap() {
-  map = new google.maps.Map(document.getElementById("map"), {
+async function initMap() {
+  const { Map, InfoWindow } = await google.maps.importLibrary("maps");
+  const { Marker } = await google.maps.importLibrary("marker");
+
+  map = new Map(document.getElementById("map"), {
     center: new google.maps.LatLng(DEFAULT_CENTER.lat, DEFAULT_CENTER.lng),
     zoom: 14,
   });
 
-  infoWindow = new google.maps.InfoWindow();
+  infoWindow = new InfoWindow();
 
   if (navigator.geolocation) {
     navigator.geolocation.getCurrentPosition(
-      ({ coords }) => {
+      async ({ coords }) => {
         currentPosition = {
           lat: coords.latitude,
           lng: coords.longitude,
@@ -100,7 +103,7 @@ function initMap() {
         service.nearbySearch(request, (results, status) => {
           if (status === google.maps.places.PlacesServiceStatus.OK) {
             results.forEach((restaurant) => {
-              const marker = new google.maps.Marker({
+              const marker = new Marker({
                 position: restaurant.geometry.location,
                 icon: ICONS["food"].icon,
                 map: map,
@@ -122,89 +125,90 @@ function initMap() {
 
         // EC: Show my location
         map.setCenter(currentPosition);
-        const myMarker = new google.maps.Marker({
+        const myMarker = new Marker({
           position: currentPosition,
           icon: ICONS["cat"].icon,
           map: map,
           zIndex: 999,
         });
 
-        sendRequest("POST", API_URL, {
-          username: API_USERNAME,
-          lat: currentPosition.lat,
-          lng: currentPosition.lng,
-        })
-          .then((responseData) => {
-            // Show cars
-            const cars = responseData
-              .map(({ username, lat, lng }) => {
-                const carPosition = new google.maps.LatLng(lat, lng);
-                const distance =
-                  google.maps.geometry.spherical.computeDistanceBetween(
-                    currentPosition,
-                    carPosition
-                  );
-                return {
-                  title: username,
-                  position: carPosition,
-                  type: "car",
-                  distance: toMiles(distance),
-                };
-              })
-              .sort((a, b) => a.distance - b.distance);
+        // EC: Show nearby cars
+        try {
+          const responseData = await sendRequest("POST", API_URL, {
+            username: API_USERNAME,
+            lat: currentPosition.lat,
+            lng: currentPosition.lng,
+          });
 
-            const markers = cars.map((car) => {
-              const marker = new google.maps.Marker({
-                position: car.position,
-                icon: ICONS["car"].icon,
-                map: map,
-              });
+          // Show cars
+          const cars = responseData
+            .map(({ username, lat, lng }) => {
+              const carPosition = new google.maps.LatLng(lat, lng);
+              const distance =
+                google.maps.geometry.spherical.computeDistanceBetween(
+                  currentPosition,
+                  carPosition
+                );
+              return {
+                title: username,
+                position: carPosition,
+                type: "car",
+                distance: toMiles(distance),
+              };
+            })
+            .sort((a, b) => a.distance - b.distance);
 
-              const kilometers = toKilometers(car.distance);
+          const markers = cars.map((car) => {
+            const marker = new Marker({
+              position: car.position,
+              icon: ICONS["car"].icon,
+              map: map,
+            });
 
-              // EC: Show car info
-              marker.addListener("click", () => {
-                infoWindow.setContent(
-                  `<div>
+            const kilometers = toKilometers(car.distance);
+
+            // EC: Show car info
+            marker.addListener("click", () => {
+              infoWindow.setContent(
+                `<div>
                     <h3>Vehicle ${car.title}</h3>
                     <p>Distance: ${car.distance} miles (${kilometers} kms) away</p>
                   </div>`
-                );
-                infoWindow.open({ anchor: marker, map });
-              });
-
-              marker.title = car.title;
-              marker.distance = car.distance;
-
-              return marker;
+              );
+              infoWindow.open({ anchor: marker, map });
             });
 
-            const closestMarker = markers[0];
-            const line = new google.maps.Polyline({
-              path: [currentPosition, closestMarker.position],
-              geodesic: true,
-              strokeColor: "#FF0000",
-              strokeOpacity: 1.0,
-              strokeWeight: 2,
-            });
-            line.setMap(map);
+            marker.title = car.title;
+            marker.distance = car.distance;
 
-            // EC: Show both miles and kilometers for accessibility
-            myMarker.addListener("click", () => {
-              infoWindow.setContent(
-                `<div>
+            return marker;
+          });
+
+          const closestMarker = markers[0];
+          const line = new google.maps.Polyline({
+            path: [currentPosition, closestMarker.position],
+            geodesic: true,
+            strokeColor: "#FF0000",
+            strokeOpacity: 1.0,
+            strokeWeight: 2,
+          });
+          line.setMap(map);
+
+          // EC: Show both miles and kilometers for accessibility
+          myMarker.addListener("click", () => {
+            infoWindow.setContent(
+              `<div>
                   <h3>Closest vehicle: ${closestMarker.title}</h3>
                   <p>Distance: ${closestMarker.distance} miles away</p>
                 </div>`
-              );
-              infoWindow.open({ anchor: myMarker, map });
-            });
-          })
-          .catch((err) => {
-            alert(err);
+            );
+            infoWindow.open({ anchor: myMarker, map });
           });
+        } catch (err) {
+          alert(err);
+        }
       },
-      (err) => {
+      async (err) => {
         handleLocationError(true, infoWindow, map.getCenter(), err.message);
       }
     );
